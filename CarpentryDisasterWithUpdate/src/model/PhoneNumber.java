2c8ab8de30ba6d2eedf361040b7f13ef5c7c8aee
lagerup <damiensheehy@hotmail.com>
2018-02-23 12:33:06 +0000

    phoneNumber class 23/02/18

---
+++ CarpentryDisasterWithUpdate/src/model/PhoneNumber.java
@@ -0,0 +1,82 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PhoneNumber {
+	@Id//primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//auto number
+	private int id;
+
+	@Column
+	private String phoneNumber;
+
+	public PhoneNumber() {}
+
+	public PhoneNumber(int id, String phoneNumber) {
+		super();
+		this.id=id;
+		this.phoneNumber=phoneNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PhoneNumber other = (PhoneNumber) obj;
+		if (id != other.id)
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PhoneNumber [id=" + id + ", phoneNumber=" + phoneNumber + "]";
+	}
+
+}
+
+
+
+
+
+
+
